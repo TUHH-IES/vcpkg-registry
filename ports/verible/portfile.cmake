4c1a6f3647386c9c03d91b6589fc96704f5ffb61
Gianluca Martino <gian21391@users.noreply.github.com>
2025-04-15 17:29:48 +0200

    Verible update.

--- ports/verible/portfile.cmake
+++ ports/verible/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO gian21391/verible
-  REF 8d7281a4f3e6da6dcadf1471600390f3b3d8cf6e
-  SHA512 58af76145fe54f984e5ba7c8404d229b9877767b06f1c4e0c5df09928f19fbc51ead59527946993009b7143d3c3449ff2071002a109318abed2b45d396daa0df
+  REF 26ec40be128e56563058574ba7fe3d851ec5c8e7
+  SHA512 ca8f3c58ccf007dc8a25f02b742123e2c053dfe875149af9e76af25641d148c3d0260a0e51af79f9a6ea468a289ccab4d531795ac63e274f60a77752dacf997c
   HEAD_REF cmake_build
 )
 
@@ -26,6 +26,7 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/common/tools" "${CURRENT_PA
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/verilog/tools/kythe/testdata/include_with_dir_test" "${CURRENT_PACKAGES_DIR}/include/verilog/tools/lint" "${CURRENT_PACKAGES_DIR}/include/verilog/tools/ls/vscode" "${CURRENT_PACKAGES_DIR}/include/verilog/tools/syntax")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/verilog/tools/kythe/testdata")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/verible/common/tools" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/diff" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/formatter" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/kythe/testdata/include_file_test" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/kythe/testdata/include_with_dir_test/include_dir" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/kythe/testdata/multi_file_test" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/lint/testdata" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/ls/vscode/images" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/ls/vscode/src" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/obfuscator" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/preprocessor" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/project" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/syntax/export_json_examples")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/kythe/testdata/include_with_dir_test" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/lint" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/ls/vscode" "${CURRENT_PACKAGES_DIR}/include/verible/verilog/tools/syntax")
 file(
   INSTALL "${SOURCE_PATH}/LICENSE"
   DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
